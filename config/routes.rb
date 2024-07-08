fa32b6e7247db637c18e3783845a9f8093890cb8
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-08 17:18:10 +0000

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get("/rock", { :controller => "zebra", :action => "giraffe" })
+
+  get("/rock", { :controller => "zebra", :action => "elephant" })
+
+  get("/rock", { :controller => "zebra", :action => "giraffe" })
 
 end
